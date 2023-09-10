b012e336d6da4f07ce7f967ccf6ac7b3626d7a20
Hartzell Max Phillips <143150623+HartzellMaxPhillips@users.noreply.github.com>
2023-09-10 17:23:52 -0600

    Challenge 1 finished

--- Basics/001 Conventions and Syntax/Challenge1.cs
+++ Basics/001 Conventions and Syntax/Challenge1.cs
@@ -1,7 +1,31 @@
 using System;
 
-public class Program {
-    public void main () {
-        
+
+
+public class Program 
+{
+    public void Main () 
+    {
+        //Here are some variable declarations as requested by the challenge
+        //For fun I will tell as to give the integers meaning.  The story may only make some sense, so be warned.
+        //The two sies of my family, the Wards and Phillips are having a contest for how many cookies they have.
+        int cookiesWard = 5;
+        int cookiesPhillips = 8;
+
+        //Somone  took a bite of the cookies and made them all sad :(
+        float cookiesW = 3.5f;
+        float cookiesP = 8.3f;
+
+        //So now they're so mad that they can fly.
+        bool flying = true;
+
+        Console.WriteLine ("Ward Cookies Before: " +cookiesWard);
+        Console.WriteLine ("Phillips Cookies Before: " +cookiesPhillips);
+        Console.WriteLine ("Ward Cookies After" +cookiesW);
+        Console.WriteLine ("Phillips Cookies After: " +cookiesP);
+        Console.WriteLine ("Flying?: " +flying);
+
+    
+
     }
 }
