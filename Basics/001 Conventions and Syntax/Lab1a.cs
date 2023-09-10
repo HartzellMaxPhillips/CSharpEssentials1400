32370c08753d1d941f43ca995235a831cc8d0c06
Hartzell Max Phillips <143150623+HartzellMaxPhillips@users.noreply.github.com>
2023-09-10 16:39:43 -0600

    Renamed Lab1aPractice to Lab1a.  Finished the Variables part of the assignment

---
+++ Basics/001 Conventions and Syntax/Lab1a.cs
@@ -0,0 +1,39 @@
+using System;
+
+namespace MyApplication
+{
+  public class Program
+  {
+    public void Main()
+    {
+      //This is a String Variable
+      string nameString = "Max";
+      Console.WriteLine("My name(String) is: " + nameString); 
+
+      //This is an integer Variable
+      int intNumber = 27;
+      Console.WriteLine("My age is: " + intNumber);
+      
+      //This is a float Variable, it holds some decimal places
+      float myFloat = 25.856f;
+      Console.WriteLine("My float is: " + myFloat);
+
+      //This is a Double Variable, it can hold a lot of Decimal places
+      double myDouble = 78.193785692;
+      Console.WriteLine("My Double: " + myDouble);
+
+      //This is a Char or Character Variable
+      char myChar = 'A';
+      Console.WriteLine("My char: " + myChar);
+
+      //This is a Boolean
+      bool myBool = true;
+      Console.WriteLine("my boolean: " + myBool);
+     }
+  }
+
+  public class variableExamples 
+  {
+
+  }
+}
