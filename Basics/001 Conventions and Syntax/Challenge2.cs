e40acfc4bdbe822258d29cfd740a30864fc3f97f
Hartzell Max Phillips <143150623+HartzellMaxPhillips@users.noreply.github.com>
2023-09-10 18:19:25 -0600

    Challenge 2 completed and fixed

--- Basics/001 Conventions and Syntax/Challenge2.cs
+++ Basics/001 Conventions and Syntax/Challenge2.cs
@@ -1,2 +1,50 @@
-//Now going with the idea of having 2 families and their cookies needing to be organized.  Let's make this more interesting shall we?
-//The challenge requests that I perform operations on numeric types
+using System;
+
+
+public class Program 
+{
+    public void Main () 
+    {
+        //This is how many cookies the familes start with
+        float cookiesWard = 10f;
+        float cookiesPhillips = 11f;
+        
+        //Finigan eats a cookie and a bite from both of them
+        float finiganEats = 1.1f;
+        cookiesWard = cookiesWard - finiganEats;
+        cookiesPhillips = cookiesPhillips - finiganEats;
+
+        //Carly eats half a cookie from the wards
+        float carlyEats = 0.5f;
+        cookiesWard = cookiesWard - carlyEats;
+
+        //The cookies get blessed by a fairy and double up and then some.
+        float fairyBless = 2.5f;
+        cookiesWard = cookiesWard * fairyBless;
+        cookiesPhillips = cookiesPhillips * fairyBless;
+
+        //Finally the cookies get divided amongs everyone evenly
+        float numFamilyMembers = 10;
+		
+		float cookiesW = cookiesWard;
+		float cookiesP = cookiesPhillips;
+
+        cookiesW = cookiesWard / numFamilyMembers;
+        cookiesP = cookiesPhillips / numFamilyMembers;
+
+        float cookiesTotal = cookiesW + cookiesP;
+
+        
+
+        //writing out the variables as requested.
+        Console.WriteLine ("Ward Cookies Before: " +cookiesWard);
+        Console.WriteLine ("Phillips Cookies Before: " +cookiesPhillips);
+        Console.WriteLine ("Ward Cookies After" +cookiesW);
+        Console.WriteLine ("Phillips Cookies After: " +cookiesP);
+        Console.WriteLine ("Phillips and Ward cookies after: " +cookiesTotal);
+
+
+    
+
+    }
+}
